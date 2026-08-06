23e03a93085d9acd5b8d7153bd9e9b654f6cab70
Adri Mourits <adri.mourits@deltares.nl>
2026-08-06 16:57:11 +0200

    Update settings.kts

--- ci/teamcity/SWAN/settings.kts
+++ ci/teamcity/SWAN/settings.kts
@@ -27,7 +27,48 @@ version = "2026.1"
 
 project {
 
+    description = "contact: BlackOps (black-ops@deltares.nl)"
+
+    params {
+        param("delft3d-user", DslContext.getParameter("delft3d-user"))
+        password("delft3d-secret", DslContext.getParameter("delft3d-secret"))
+
+        param("s3_dsctestbench_accesskey", DslContext.getParameter("s3_dsctestbench_accesskey"))
+        password("s3_dsctestbench_secret", "credentialsJSON:7e8a3aa7-76e9-4211-a72e-a3825ad1a160")
+
+        param("dvc_testbench_accesskey", DslContext.getParameter("dvc_testbench_accesskey"))
+        password("dvc_testbench_secret", DslContext.getParameter("dvc_testbench_secret"))
+
+        param("nexus_username", DslContext.getParameter("nexus_username"))
+        password("nexus_password", DslContext.getParameter("nexus_password"))
+        password("nexus_nuget_apikey", DslContext.getParameter("nexus_nuget_apikey"))
+        param("nexus_iq_username", DslContext.getParameter("nexus_iq_username"))
+        password("nexus_iq_password", DslContext.getParameter("nexus_iq_password"))
+        param("env.UV_INDEX_URL", "https://%nexus_username%:%nexus_password%@internal-artifacts.deltares.nl/repository/python-internal/simple/")
+        param("product", "dummy_value")
+
+    }
+
+
     buildType(Build)
+
+
+    features {
+        dockerRegistry {
+            id = "DOCKER_REGISTRY_DELFT3D"
+            name = "Docker Registry Delft3d"
+            url = "https://containers.deltares.nl/"
+            userName = "%delft3d-user%"
+            password = "%delft3d-secret%"
+        }
+        feature {
+            type = "OAuthProvider"
+            param("displayName", "Keeper Vault Delft3d")
+            param("secure:client-secret", "credentialsJSON:bcf00886-4ae4-4c0a-9701-4e37efab8504")
+            param("providerType", "teamcity-ksm")
+        }
+    }
+
 }
 
 object Build : BuildType({
