8104dc634ba6969853c0f36516ebc1de6f4fc29c
Adri Mourits <adri.mourits@deltares.nl>
2026-08-18 18:13:00 +0200

    settings.kts: Second try: 2 subprojects: Windows, Linux

--- ci/teamcity/SWAN/settings.kts
+++ ci/teamcity/SWAN/settings.kts
@@ -26,7 +26,6 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2026.1"
 
 project {
 
@@ -42,20 +41,22 @@ project {
         password("nexus_iq_password", DslContext.getParameter("nexus_iq_password"))
         param("env.UV_INDEX_URL", "https://%nexus_username%:%nexus_password%@internal-artifacts.deltares.nl/repository/python-internal/simple/")
         param("product", "dummy_value")
-
-        // Windows only:
-        param("container.tag", "vs2022-intel2024-ltsc2025")
-        param("nexus_conan_username", DslContext.getParameter("nexus_conan_username"))
-        password("nexus_conan_password", DslContext.getParameter("nexus_conan_password"))
-        param("conan_build_option", "--build-missing")
-        param("env.CONAN_HOME", "C:/conan-cache")
-
     }
 
     template(TemplateDockerRegistry)
 
+    subProject {
+        id("Linux")
+        name = "Linux"
+        buildType(LinuxBuild)
+    }
 
-    buildType(Build)
+
+    subProject {
+        id("Windows")
+        name = "Windows"
+        buildType(WindowsBuild)
+    }
 
 
     features {
@@ -77,10 +78,19 @@ project {
 }
 
 object Build : BuildType({
-    name = "Build"
+    name = "WindowsBuild"
+
+    // Windows only:
+    param("container.tag", "vs2022-intel2024-ltsc2025")
+    param("nexus_conan_username", DslContext.getParameter("nexus_conan_username"))
+    password("nexus_conan_password", DslContext.getParameter("nexus_conan_password"))
+    param("conan_build_option", "--build-missing")
+    param("env.CONAN_HOME", "C:/conan-cache")
 
     vcs {
         root(DslContext.settingsRoot)
+        cleanCheckout = true
+        checkoutDir = "swanbuild-win64"
     }
 
     steps {
@@ -95,6 +105,41 @@ object Build : BuildType({
             dockerImage = "containers.deltares.nl/delft3d-dev/delft3d-buildtools-windows:%container.tag%"
             dockerPull = true
             dockerRunParameters = "--memory %teamcity.agent.hardware.memorySizeMb%m --cpus %teamcity.agent.hardware.cpuCount% --mount type=volume,source=delft3d-conan-cache,target=C:/conan-cache -e CONAN_LOGIN_USERNAME_DELFT3D_CONAN_DEV=%nexus_conan_username% -e CONAN_PASSWORD_DELFT3D_CONAN_DEV=%nexus_conan_password%"
+        }
+    }
+})
+
+object Build : BuildType({
+    name = "LinuxBuild"
+
+    vcs {
+        root(DslContext.settingsRoot)
+        cleanCheckout = true
+        checkoutDir = "swanbuild-lnx64"
+    }
+
+    steps {
+        script {
+            name = "Build"
+            id = "DockerCommand"
+            scriptContent = """
+                #!/usr/bin/env bash
+                source /etc/bashrc
+                set -eo pipefail
+                export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:${'$'}PKG_CONFIG_PATH
+                export LD_LIBRARY_PATH=/usr/local/lib:${'$'}LD_LIBRARY_PATH
+                export CMAKE_PREFIX_PATH=/usr/local:${'$'}CMAKE_PREFIX_PATH
+                export CMAKE_INCLUDE_PATH=/usr/local/include:${'$'}CMAKE_INCLUDE_PATH
+                export CMAKE_LIBRARY_PATH=/usr/local/lib:${'$'}CMAKE_LIBRARY_PATH
+
+                # Initialize Conan and install pre-built dependencies from Nexus
+                python run_conan.py initialize deltares --ci
+                python build.py --build --ci
+            """.trimIndent()
+            dockerImage = "containers.deltares.nl/delft3d-dev/delft3d-third-party-libs:%dep.${LinuxThirdPartyLibs.id}.env.IMAGE_TAG%"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+            dockerRunParameters = "--rm --mount type=volume,source=delft3d-conan-cache,target=/conan-cache -e CONAN_LOGIN_USERNAME_DELFT3D_CONAN_DEV=%nexus_conan_username% -e CONAN_PASSWORD_DELFT3D_CONAN_DEV=%nexus_conan_password%"
+            dockerPull = true
         }
     }
 })
