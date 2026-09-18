0cd62fa7038ee972c9e1abf1bfbe00bed886fe33
Mihai Pricope <mihai.pricope@deltares.nl>
2026-09-18 21:36:03 +0200

    fixed container

--- ci/teamcity/SWAN/windows/build.kt
+++ ci/teamcity/SWAN/windows/build.kt
@@ -63,7 +63,7 @@ object WindowsBuild : BuildType({
                 powershell -NoProfile -ExecutionPolicy Bypass -File ci\teamcity\SWAN\windows\scripts\test_all_local.ps1 C:\workspace "%teamcity.build.branch%" "41.51.9CONAN"
                 if %%errorlevel%% neq 0 exit /b %%errorlevel%%
             """.trimIndent()
-            dockerImage = "containers.deltares.nl/swan-dev/delft3d-buildtools-windows:%container.tag%"
+            dockerImage = "containers.deltares.nl/swan-dev/swan-buildtools-windows:%container.tag%"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Windows
             dockerPull = true
             dockerRunParameters = "--memory %teamcity.agent.hardware.memorySizeMb%m --cpus %teamcity.agent.hardware.cpuCount% --mount type=volume,source=delft3d-conan-cache,target=C:/conan-cache -e CONAN_LOGIN_USERNAME_DELFT3D_CONAN_DEV=%nexus_conan_username% -e CONAN_PASSWORD_DELFT3D_CONAN_DEV=%nexus_conan_password% -e SVN_USER_NAME=%svn_username% -e SVN_PASSWORD"
