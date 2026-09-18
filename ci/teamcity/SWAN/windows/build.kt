210a9f1bef640edf4ef17771e35692baf8381f21
Mihai Pricope <mihai.pricope@deltares.nl>
2026-09-18 21:34:41 +0200

    also for build

--- ci/teamcity/SWAN/windows/build.kt
+++ ci/teamcity/SWAN/windows/build.kt
@@ -28,6 +28,9 @@ object WindowsBuild : BuildType({
         param("build_type", "Release")
         param("nexus_conan_username", DslContext.getParameter("nexus_conan_username"))
         password("nexus_conan_password", DslContext.getParameter("nexus_conan_password"))
+        param("svn_username", DslContext.getParameter("svn_username"))
+        password("svn_password", DslContext.getParameter("svn_password"))
+        password("env.SVN_PASSWORD", DslContext.getParameter("svn_password"))
     }
 
     vcs {
@@ -57,18 +60,13 @@ object WindowsBuild : BuildType({
             scriptContent = """
                 call C:\set-env.cmd
 
-                echo === Test tooling diagnostics ===
-                where svn.exe 2>nul || echo [not found] svn.exe
-                svn --version --quiet 2>nul || echo [not available] svn --version
-                where uv.exe 2>nul || echo [not found] uv.exe
-                uv pip --version 2>nul || echo [not available] uv pip --version
-                echo === End test tooling diagnostics ===
-
+                powershell -NoProfile -ExecutionPolicy Bypass -File ci\teamcity\SWAN\windows\scripts\test_all_local.ps1 C:\workspace "%teamcity.build.branch%" "41.51.9CONAN"
+                if %%errorlevel%% neq 0 exit /b %%errorlevel%%
             """.trimIndent()
             dockerImage = "containers.deltares.nl/swan-dev/delft3d-buildtools-windows:%container.tag%"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Windows
             dockerPull = true
-            dockerRunParameters = "--memory %teamcity.agent.hardware.memorySizeMb%m --cpus %teamcity.agent.hardware.cpuCount% --mount type=volume,source=delft3d-conan-cache,target=C:/conan-cache -e CONAN_LOGIN_USERNAME_DELFT3D_CONAN_DEV=%nexus_conan_username% -e CONAN_PASSWORD_DELFT3D_CONAN_DEV=%nexus_conan_password%"
+            dockerRunParameters = "--memory %teamcity.agent.hardware.memorySizeMb%m --cpus %teamcity.agent.hardware.cpuCount% --mount type=volume,source=delft3d-conan-cache,target=C:/conan-cache -e CONAN_LOGIN_USERNAME_DELFT3D_CONAN_DEV=%nexus_conan_username% -e CONAN_PASSWORD_DELFT3D_CONAN_DEV=%nexus_conan_password% -e SVN_USER_NAME=%svn_username% -e SVN_PASSWORD"
         }
     }
 
